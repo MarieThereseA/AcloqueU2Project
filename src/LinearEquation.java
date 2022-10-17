e4b265b7a6e3c8ae291c02f29abe5052d1689112
student <evelyns60@nycstudents.net>
2022-10-17 12:34:22 -0400

    uu

--- src/LinearEquation.java
+++ src/LinearEquation.java
@@ -1,2 +1,29 @@
+import java.text.DecimalFormat;
 public class LinearEquation {
+    DecimalFormat formatter = new DecimalFormat("#.##");
+
+    private int x1;
+    private int x2;
+    private int y1;
+    private int y2;
+
+    public LinearEquation(int x1, int y1, int x2, int y2){
+        this.x1 = x1;
+        this.y1 = y1;
+        this.x2 = x2;
+        this.y2 = y2;
+    }
+
+    public String Slope(){
+        double numerator = y2 - y1;
+        double denominator = x2 - x1;
+        String slope = numerator / denominator + "";
+        slope = formatter.format(slope);
+        return slope;
+    }
+
+    public String Distance(){
+        double Distance = Math.Math.sqrt(x2-x1) + Math.sqrt(y2-y1)
+        return dist;
+    }
 }
--- src/LinearEquationRunner.java
+++ src/LinearEquationRunner.java
@@ -5,11 +5,31 @@ public class LinearEquationRunner {
 
         System.out.println("Welcome!");
         System.out.print("Enter coordinate 1: ");
-        String coord1 = scan.nextLine();
+        String one = scan.nextLine();
 
         System.out.print("Enter coordinate 2: ");
-        String coord2 = scan.nextLine();
+        String two = scan.nextLine();
 
-        
+        one = one.substring(1,2) + " " + one.substring(one.length() - 2, one.length() - 1);
+        two = two.substring(1,2) + " " + two.substring(two.length() - 2, two.length() - 1);
+
+        System.out.println(one);
+        System.out.println(two);
+
+        String firstX = one.substring(0,1);
+        String secondX = two.substring(0,1);
+        String firstY = two.substring(2);
+        String secondY = one.substring(2);
+
+
+        int x1 = Integer.parseInt(firstX);
+        int x2 = Integer.parseInt(secondX);
+        int y1 = Integer.parseInt(firstY);
+        int y2 = Integer.parseInt(secondY);
+
+        System.out.println(x1);
+        System.out.println(x2);
+        System.out.println(y1);
+        System.out.println(y2);
     }
 }
