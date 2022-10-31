44438cd716aaf1beb90ca6636fc826b8587458e4
MarieThereseA <ematetelove@gmail.com>
2022-10-31 00:14:48 -0400

    final (i think)

--- src/LinearEquation.java
+++ src/LinearEquation.java
@@ -16,6 +16,9 @@ public class LinearEquation {
         double numerator = y2 - y1;
         double denominator = x2 - x1;
         double slope = numerator / denominator ;
+        if (numerator == 0){
+            slope = 0;
+        }
         slope = roundToHundred(slope);
         return slope;
     }
@@ -27,7 +30,12 @@ public class LinearEquation {
 
     public double yIntercept(){
         double m = slope();
-        double intercept = -1 * (m * x1) + y1;
+        double intercept = ((m * x1) - y1);
+        if (intercept == 0){ // Making sure -0 isn't printed
+
+        }else {
+            intercept /= -1;
+        }
         intercept = roundToHundred(intercept);
         return intercept;
     }
@@ -38,21 +46,38 @@ public class LinearEquation {
         int nume = y2 - y1;
         int deno = x2 - x1;
         String equation = "";
-        if (deno < 0) {
-            deno = Math.abs(deno);
-            nume = nume * -1;
-            if (nume % deno == 0) {
-                m = nume / deno;
-                equation = "y = " + m + "x + " + b;
-            }else {
-                equation = "y = " + nume + "/" + deno + "x + " + b;
-            }
-        }
-        if (nume % deno == 0 ){
-            m = nume / deno;
-            equation = "y = " + m + "x + " + b;
+        if (slope() == 0){ // Checking for horizontal line
+            equation = "y = " + y1;
         }else {
-            equation = "y = " + nume + "/" + deno + "x + " + b;
+            if (deno < 0 && nume > 0) { // Moving negative sign
+                deno = Math.abs(deno);
+                nume = nume * -1;
+            }
+            if (nume % deno == 0) { // Checking for decimal slope
+                m = nume / deno;
+                if (m == 1) {
+                    equation = "y = x ";
+                } else if (m == -1) {
+                    equation = "y = -x ";
+                } else {
+                    equation = "y = " + m + "x ";
+                }
+                if (b < 0) {
+                    equation += "- " + -1 * b;
+                } else if (b > 0) {
+                    equation += "+ " + b;
+                } else {
+                }
+            } else { // Printing slope as a fraction
+                equation = "y = " + nume + "/" + deno + "x ";
+                if (b < 0) {
+                    equation += "- " + -1 * b;
+                } else if (b == 0) {
+
+                } else {
+                    equation += "+ " + b;
+                }
+            }
         }
         return equation;
     }
@@ -61,28 +86,38 @@ public class LinearEquation {
         double y = slope() * xVal + yIntercept();
         y = roundToHundred(y);
         String coord = "(" + xVal + ", " + y + ")";
-        return "Coordinate for x: " + coord;
+        return coord;
     }
 
     // rounding tool
     public double roundToHundred(double num){
+        boolean numNeg = false;
         double roundedNum = 0;
         String number = num + "";
         int decimal = number.indexOf(".");
-        if (decimal == -1){
+        if (decimal == -1){ // Checking if number is a whole number
             return num;
-        }else if (decimal + 4 < number.length()) {
+        }else if (decimal + 4 < number.length()) { // Checking if number exceeds hundredth's place
             int wholeNum = Integer.parseInt(number.substring(0, decimal));
-            int tenth = Integer.parseInt(number.substring(decimal + 1, decimal + 2));
+            if (wholeNum < 0){ // Temporarily removing negative sign to prevent calculation errors
+                wholeNum *= -1;
+                numNeg = true; // Tracker variable
+            }else if (wholeNum == 0 && number.indexOf("-") != -1){
+                numNeg = true;
+            }
+            int tenth = Integer.parseInt(number.substring(decimal + 1, decimal + 2)); //Finding values for various decimal places
             int hundredth = Integer.parseInt(number.substring(decimal + 2, decimal + 3));
             int thousandth = Integer.parseInt(number.substring(decimal + 3, decimal + 4));
-            double decimalPlace = (tenth * 10.0) + hundredth + (thousandth / 10.0);
-            decimalPlace = Math.round(decimalPlace);
-            if (decimalPlace == 100) {
+            double decimalPlace = (tenth * 10.0) + hundredth + (thousandth / 10.0); //Making decimal place values a whole number
+            decimalPlace = Math.round(decimalPlace); // rounding decimal place
+            if (decimalPlace == 100) { // Checking if decimal place rounds to 100
                 wholeNum++;
                 roundedNum = wholeNum;
             } else if (decimalPlace < 100) {
-                roundedNum = wholeNum + (decimalPlace / 100);
+                roundedNum = wholeNum + (decimalPlace / 100); // Adding rounded decimal place to whole number
+                if (numNeg){
+                    roundedNum *= -1; // Putting negative sign back
+                }
             }
         }else {
             return num;
@@ -91,7 +126,7 @@ public class LinearEquation {
     }
 
     public String lineInfo(){
-        String info = "The original points are: (" + x1 + ", " + y1 + ") and (" + x2 + ", " + y2 + ")\n" ;
+        String info = "The two points are: (" + x1 + ", " + y1 + ") and (" + x2 + ", " + y2 + ")\n" ;
         info += "The equation of the line between these points is : " + equation() + "\n";
         info += "The slope of the line is: " + slope() + "\n";
         info += "The y-intercept of the line is: " + yIntercept() + "\n";
--- src/LinearEquationRunner.java
+++ src/LinearEquationRunner.java
@@ -3,6 +3,7 @@ public class LinearEquationRunner {
     public static void main(String[] args){
         Scanner scan = new Scanner(System.in);
 
+        // Welcome + Coordinate input
         System.out.println("Welcome!");
         System.out.print("Enter coordinate 1: ");
         String one = scan.nextLine();
@@ -10,55 +11,30 @@ public class LinearEquationRunner {
         System.out.print("Enter coordinate 2: ");
         String two = scan.nextLine();
 
-        one = one.substring(1,2) + " " + one.substring(one.length() - 2, one.length() - 1);
-        two = two.substring(1,2) + " " + two.substring(two.length() - 2, two.length() - 1);
-
-        System.out.println(one);
-        System.out.println(two);
-
-        String firstX = one.substring(0,1);
-        String secondX = two.substring(0,1);
-        String firstY = two.substring(2);
-        String secondY = one.substring(2);
-
+        // Parsing x and y values
+        String firstX = one.substring(one.indexOf("(") + 1, one.indexOf(","));
+        String secondX = two.substring(two.indexOf("(") + 1, two.indexOf(","));
+        String firstY = one.substring(one.indexOf(" ") + 1, one.indexOf(")"));
+        String secondY = two.substring(two.indexOf(" ") + 1 , two.indexOf(")"));
 
         int x1 = Integer.parseInt(firstX);
         int x2 = Integer.parseInt(secondX);
         int y1 = Integer.parseInt(firstY);
         int y2 = Integer.parseInt(secondY);
 
+        // Checking for vertical line
         if (x1==x2){
-            System.out.println();
+            System.out.println("These points are on a vertical line: x = " + x1);
         }else {
-            LinearEquation eq = new LinearEquation(x1, x2, y1, y2);
+            LinearEquation eq = new LinearEquation(x1, y1, x2, y2);
             System.out.println(eq.lineInfo());
 
+            //X value input
             System.out.print("Enter a value for x: ");
             String x = scan.nextLine();
-            int xCoord = Integer.parseInt(x);
+            double xCoord = Double.parseDouble(x);
 
             System.out.println(eq.coordinateForX(xCoord));
         }
-
-
-
-        // Testing
-        int x1 = -1;
-        int y1 = 5;
-        int x2 = 3;
-        int y2 = 10;
-        LinearEquation equation = new LinearEquation(x1, y1, x2, y2);
-        System.out.println("Equation: " + equation.equation());
-        System.out.println("Slope: " + equation.slope());
-        System.out.println("y-intercept: " + equation.yIntercept());
-        System.out.println("Distance: " + equation.distance());
-        System.out.println();
-        System.out.println("----- Line info -----");
-        System.out.println(equation.lineInfo());
-        System.out.println();
-        double testX = 4;
-        System.out.println("Coordinate for x: " + equation.coordinateForX(testX));
-
-
     }
 }
